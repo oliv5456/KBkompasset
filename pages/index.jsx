aea6f87f0e51577486b9f18db49a6e7de8f42dd7
Freddiiy <frederikgaller@live.dk>
2022-02-25 21:19:31 +0100

    asd

--- pages/index.jsx
+++ pages/index.jsx
@@ -12,6 +12,17 @@ const Home = () => {
   );
 };
 
+/*
+getDirection(lat1, lng1, lat2, lng2) {
+	var PI = Math.PI;
+	var dTeta = Math.log(Math.tan((lat2/2)+(PI/4))/Math.tan((lat1/2)+(PI/4)));
+	var dLon = Math.abs(lng1-lng2);
+	var teta = Math.atan2(dLon,dTeta);
+	var direction = Math.round((teta * 180 / Math.PI));
+	return direction;
+}
+*/
+
 function Compass() {
   const [errorText, setErrorText] = useState("");
   const [latitude, setLatitude] = useState(0);
