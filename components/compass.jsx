a91d708757430ab8f09c9ff3dd04dc76f000d062
Freddiiy <frederikgaller@live.dk>
2022-02-27 13:50:05 +0100

    direction alpha reverse

--- components/compass.jsx
+++ components/compass.jsx
@@ -35,11 +35,22 @@ export default function Compass({ targetLat, targetLon }) {
   }
 
   const getDirection = useCallback(() => {
+    /*
+getDirection(lat1, lng1, lat2, lng2) {
+	var PI = Math.PI;
+	var dTeta = Math.log(Math.tan((lat2/2)+(PI/4))/Math.tan((lat1/2)+(PI/4)));
+	var dLon = Math.abs(lng1-lng2);
+	var teta = Math.atan2(dLon,dTeta);
+	var direction = Math.round((teta * 180 / Math.PI));
+	return direction;
+}
+*/
+
     let dTeta = Math.log(
-      Math.tan(targetLat / 2) +
-        Math.PI / 4 / Math.tan(latitude / 2 + Math.PI / 4)
+      Math.tan(latitude / 2) +
+        Math.PI / 4 / Math.tan(targetLat / 2 + Math.PI / 4)
     );
-    let dLon = Math.abs(longitude - targetLon);
+    let dLon = Math.abs(targetLon - longitude);
     let teta = Math.atan2(dLon, dTeta);
     let direction = Math.round((teta * 180) / Math.PI);
 
