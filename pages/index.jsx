46e387b3fac54d77c7a05403cd7c689fc3343739
Freddiiy <frederikgaller@live.dk>
2022-03-01 09:23:00 +0100

    browser view prettier

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,22 +5,28 @@ import compassImg from "../public/compass.svg";
 import styled, { CompassTheme } from "styled-components";
 import Compass from "../components/compass";
 import { BrowserView, MobileOnlyView } from "react-device-detect";
-import { Container, Row } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 
 
 const Home = () => {
   return (
     <>
-	<BrowserView>
-		<Container fluid className="bg-danger vh-100">
-			<div className="flex-column">
-				<h1 className="text-center text-white align-middle">Du skal bruge iphone eller android for at bruge kompasset :(</h1>
-			</div>
-		</Container>
-	</BrowserView>
-	<MobileOnlyView>
+	<MobileOnlyView className="bg-body">
       <Compass targetLat={55.7864419} targetLon={12.5234279} />
 	</MobileOnlyView>
+	<BrowserView>
+		<Container fluid className="bg-danger vh-100 m-auto d-flex justify-content-center align-items-center">
+			<Row>
+				<Col md={2}></Col>
+				<Col md={8} className="text-center">
+					<div>
+						<h1 className="text-center text-white">Du skal bruge iphone eller android for at bruge kompasset :(</h1>
+					</div>
+				</Col>
+				<Col md={2}></Col>
+			</Row>
+		</Container>
+	</BrowserView>
     </>
   );
 };
